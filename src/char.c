ba3ba0c8d6ae906fde2e01025c7eb3168fd516ed
LucasTavaresA <tavares.lassuncao@gmail.com>
2024-03-24 18:33:26 -0300

    fix: char.c functions colliding in android build

--- src/char.c
+++ src/char.c
@@ -1,26 +1,31 @@
-int isalpha(int c)
+#ifndef CHAR_C
+#define CHAR_C
+
+static int isalpha(int c)
 {
   return ((unsigned)c | 32) - 'a' < 26;
 }
 
-int isdigit(int c)
+static int isdigit(int c)
 {
   return (unsigned)c - '0' < 10;
 }
 
-int isalnum(int c)
+static int isalnum(int c)
 {
   return isalpha(c) || isdigit(c);
 }
 
-int isupper(int c)
+static int isupper(int c)
 {
   return (unsigned)c - 'A' < 26;
 }
 
-int tolower(int c)
+static int tolower(int c)
 {
   if (isupper(c))
     return c | 32;
   return c;
 }
+
+#endif  // CHAR_C
